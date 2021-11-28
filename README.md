387ba716594be47047dcfaa7914ae9ce568e0930
Ozodbek Oripjonov <ozod3837898@gmail.com>
2021-11-28 20:41:20 +0500

    react boilerplate is ready

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# react-redux-webpack-boilerplate
+## Installation
+
+1. Clone this repo in `git clone https://github.com/imranhsayed/react-workshop`
+
+2. `cd react-pwa-workbox-example`
+
+3. Run `npm install`
+3. Run `npm run dev`
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const { InjectManifest } = require('workbox-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MinCssExtractPlugin = require('mini-css-extract-plugin');
+
+const webpackPlugins = [
+  new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'public/index.html'),
+    filename: 'index.html'
+  }),
+  new MinCssExtractPlugin(),
+  new Dotenv({
+    path: './.env', // Path to .env file (this is the default)
+    systemvars: true
+  }),
+  new CopyPlugin({
+    patterns: [
+      { from: './src/favicon.ico', to: '' },
+      { from: './src/manifest.json', to: '' },
+      { from: './src/logo192.png', to: '' },
+      { from: './src/logo512.png', to: '' }
+    ]
+  })
+];
+
+if ('production' === process.env.NODE_ENV) {
+  webpackPlugins.push(
+    new InjectManifest({
+      swSrc: './src/src-sw.js',
+      swDest: 'sw.js'
+    })
+  );
+}
+
+module.exports = {
+  context: __dirname,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '/'
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css?$/,
+        use: [MinCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|j?g|svg|gif)?$/,
+        use: 'file-loader?name=./images/[name].[ext]'
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: webpackPlugins
+};
